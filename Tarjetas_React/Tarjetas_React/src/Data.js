199160f77489611dd1c82a2d8ebac4f514698c17
aldo puello <apuello9@cuc.edu.co>
2023-08-14 14:54:20 -0500

    Update Data.js

--- Tarjetas_React/Tarjetas_React/src/Data.js
+++ Tarjetas_React/Tarjetas_React/src/Data.js
@@ -1,51 +1,50 @@
 export const simpsonsCharacter = [
-    {
-      id: 1,
-      title: "Homero Simpson",
-      description:
-        "Homer Jay Simpson es un personaje ficticio protagonista de la serie de televisión de dibujos animados Los Simpson. ",
-      image:
-        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2017/08/homer-simpson.jpeg?itok=8Cr6PCel",
-    },
-    {
-      id: 2,
-      title: "Bart Simpson",
-      description:
-        "Bartholomew J. Simpson​​ es uno de los personajes ficticios protagonistas de la serie de televisión de dibujos animados Los Simpson.",
-      image:
-        "https://mantenlosimpleblog.files.wordpress.com/2014/06/bart-simpson-principal.png",
-    },
-    {
-      id: 3,
-      title: "Lisa Simpson",
-      description:
-        "Lisa Marie Simpson​​ es un personaje de la serie de televisión de dibujos animados Los Simpson. Es la hija mediana.",
-      image:
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-simpsons-lisa-1589201259.jpg?crop=0.567xw:1.00xh;0.241xw,0&resize=1200:*",
-    },
-    {
-      id: 4,
-      title: "Marge Simpson",
-      description:
-        "Marjorie Bouvier Marge Simpson es un personaje ficticio de la serie de televisión de dibujos animados Los Simpson.",
-      image:
-        "https://i.discogs.com/WI38UGun7TKKNg04MTHK0yV-I1Yb75sdJ_t0GEermmo/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTI2MzMx/MzAtMTMzNTcyMDM5/MC5wbmc.jpeg",
-    },
-    {
-      id: 5,
-      title: "Ned Flanders",
-      description:
-        "Nedward Flanders, ​ más conocido como Ned, es un personaje ficticio de la serie de televisión de dibujos animados Los Simpson.",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuKGF3E_nG35hCYfs7PZ56NJ06QR4hzvY_20tSdXri2w&s",
-    },
-    {
-      id: 6,
-      title: "Milhouse Van Houten",
-      description:
-        "Milhouse Mussolini Van Houten es un personaje ficticio de la serie animada Los Simpson.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/1/11/Milhouse_Van_Houten.png",
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "Neymar JR",
+    description:
+      "💣BOMBA de Los propietarios del Chelsea y PSG almorzaron este día martes en París y uno de los temas sobre la mesa fue Neymar JR (31|🇧🇷)",
+    image:
+      "https://pbs.twimg.com/media/FpCAOTpXEAstSef.jpg:large",
+  },
+  {
+    id: 2,
+    title: "Bart Simpson",
+    description:
+      "Bartholomew J. Simpson​​ es uno de los personajes ficticios protagonistas de la serie de televisión de dibujos animados Los Simpson.",
+    image:
+      "https://mantenlosimpleblog.files.wordpress.com/2014/06/bart-simpson-principal.png",
+  },
+  {
+    id: 3,
+    title: "Lisa Simpson",
+    description:
+      "Lisa Marie Simpson​​ es un personaje de la serie de televisión de dibujos animados Los Simpson. Es la hija mediana.",
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-simpsons-lisa-1589201259.jpg?crop=0.567xw:1.00xh;0.241xw,0&resize=1200:*",
+  },
+  {
+    id: 4,
+    title: "Marge Simpson",
+    description:
+      "Marjorie Bouvier Marge Simpson es un personaje ficticio de la serie de televisión de dibujos animados Los Simpson.",
+    image:
+      "https://i.discogs.com/WI38UGun7TKKNg04MTHK0yV-I1Yb75sdJ_t0GEermmo/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTI2MzMx/MzAtMTMzNTcyMDM5/MC5wbmc.jpeg",
+  },
+  {
+    id: 5,
+    title: "Ned Flanders",
+    description:
+      "Nedward Flanders, ​ más conocido como Ned, es un personaje ficticio de la serie de televisión de dibujos animados Los Simpson.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuKGF3E_nG35hCYfs7PZ56NJ06QR4hzvY_20tSdXri2w&s",
+  },
+  {
+    id: 6,
+    title: "Milhouse Van Houten",
+    description:
+      "Milhouse Mussolini Van Houten es un personaje ficticio de la serie animada Los Simpson.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/1/11/Milhouse_Van_Houten.png",
+  },
+];
